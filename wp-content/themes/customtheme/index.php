9f7fb46aca001a7debb44e26e0e369544fb75795
chetansharma10 <sparkycks10@gmail.com>
2023-01-02 10:43:27 +0530

    post-loop implemented

--- wp-content/themes/customtheme/index.php
+++ wp-content/themes/customtheme/index.php
@@ -1,3 +1,14 @@
 <?= get_header() ?>
-<h1>Index </h1>
+
+    <?php
+    if (have_posts()) {
+        while (have_posts()) : the_post(); ?>
+            <h3><?= the_title(); ?></h3>
+            <small>Posted on <?= the_time('F j,Y')?> at <?= the_time('g:i a')?>  <?= the_category(); ?></small>
+            <p><?= the_content();?></p>
+            <hr>
+    <?php
+        endwhile;
+    }
+    ?>
 <?= get_footer() ?>
