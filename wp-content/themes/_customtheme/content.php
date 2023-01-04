c65ca7fd991d5d0d551a5d1bafa62ec5135225d6
chetansharma10 <sparkycks10@gmail.com>
2023-01-04 11:00:52 +0530

    Done

--- wp-content/themes/_customtheme/content.php
+++
@@ -1,4 +0,0 @@
-<h3><?= the_title(); ?></h3>
-<small>Posted on <?= the_time('F j,Y') ?> at <?= the_time('g:i a') ?> <?= the_category(); ?></small>
-<p><?= the_content(); ?></p>
-<hr>
--- wp-content/themes/customtheme/index.php
+++ wp-content/themes/customtheme/index.php
@@ -2,6 +2,25 @@
 <?= get_header() ?>
     
     <div class="container">
+
+        <?php
+        if (have_posts()) {
+            while (have_posts()):the_post(); ?>
+                 <!-- Reading the posts -->
+                 <?=var_dump(get_post_format())?>
+                 <?php
+                    if(get_post_format()){
+                        get_template_part('template-parts/content/content', get_post_format());
+                    }
+                    else{
+                        get_template_part('template-parts/pages/page',get_page_uri());
+                    }
+                ?>
+        <?php
+            endwhile;
+        }
+        ?>
+
     </div>
 
 <!-- Including footer.php -->
