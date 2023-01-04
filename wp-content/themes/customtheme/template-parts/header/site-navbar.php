6cc979d7e2d6ea122992bf18df4af3f8e1a5aaae
chetansharma10 <sparkycks10@gmail.com>
2023-01-04 14:11:28 +0530

    dn

--- wp-content/themes/customtheme/template-parts/header/site-navbar.php
+++ wp-content/themes/customtheme/template-parts/header/site-navbar.php
@@ -1,12 +1,20 @@
+
+ <?php
+       
+        $custom_logo_id = get_theme_mod('custom_logo');
+        $logo = wp_get_attachment_image_src($custom_logo_id);
+        $websiteName =  get_bloginfo('name');
+      
+?>
  <!-- Including Nav Menus -->
  <nav class="navbar navbar-expand-lg navbar-light bg-light">
    <div class="container-fluid">
      <a class="navbar-brand" href="#">
-       <?php
-        $custom_logo_id = get_theme_mod('custom_logo');
-        $logo = wp_get_attachment_image_src($custom_logo_id);
-       ?>
-       <img width="40px" src="<?= $logo[0];?>">
+        <div class="d-flex align-items-center">
+           <img width="35px" height="35px" src="<?= $logo[0];?>">
+           <span class="text-uppercase fw-bolder"><?=$websiteName?></span>
+        </div>
+       
      </a>
      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
        <span class="navbar-toggler-icon"></span>
