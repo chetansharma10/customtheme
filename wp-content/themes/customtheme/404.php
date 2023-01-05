b6c8889ae5cebed6e642bf540e66b7c2f0097205
chetansharma10 <sparkycks10@gmail.com>
2023-01-05 11:09:50 +0530

    Done with 404

---
+++ wp-content/themes/customtheme/404.php
@@ -0,0 +1,16 @@
+<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<header class="page-header">
+				<h1 class="page-title"><?php _e( 'Not Found', 'customTheme' ); ?></h1>
+			</header>
+
+			<div class="page-wrapper">
+				<div class="page-content">
+					<h2><?php _e( 'This is somewhat embarrassing, isn’t it?', 'customTheme' ); ?></h2>
+					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'customTheme' ); ?></p>
+				</div><!-- .page-content -->
+			</div><!-- .page-wrapper -->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
--- wp-content/themes/customtheme/template-parts/content/content-aside.php
+++ wp-content/themes/customtheme/template-parts/content/content-aside.php
@@ -1,4 +1,6 @@
-<h3> ASIDE <?= the_title(); ?></h3>
+<h3><a href="<?= get_permalink($id);?>">
+ASIDE <?= the_title(); ?>
+</a></h3>
     <small>Posted on <?= the_time('F j,Y') ?> at <?= the_time('g:i a') ?> <?= the_category(); ?></small>
     <p><?= the_content(); ?></p>
 <hr>
